4f06fa6d8dd4bf286971c9093aed1e886123fd4e
AnujStha <40173983+AnujStha@users.noreply.github.com>
2019-10-02 19:14:23 +0545

    cell programmed

---
+++ RobocalypseC/Assets/models/Environment/level1/models/cell/cellDoor.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+public class cellDoor : Switchable
+{
+    private bool opened;
+    public override void On()
+    {
+        Debug.Log("called");
+        GetComponent<Animation>().Play();
+        base.On();
+
+    }
+}
